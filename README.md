b2dc743eb26600802b6388c3076c4ce368fdeca3
Harika naidu <harika.naidu@zoocasa.com>
2023-03-26 22:56:32 -0400

    handle deadlock

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # go-lang-masterclass
 https://www.udemy.com/course/backend-master-class-golang-postgresql-kubernetes/
+
+## Docker commands
+### To run psql console in terminal
+`docker exec -it postgres12 psql -U root -d simple_bank`
+
+
+## Useful links
+
+- [Postgres Lock Monitoring - To look for blocked queries and what's blocking them](https://wiki.postgresql.org/wiki/Lock_Monitoring) <br/>
+  ```
+  SELECT
+        a.application_name,
+        l.relation::regclass,
+        l.transactionid,
+        l.mode,
+        l.locktype,
+        l.GRANTED,
+        a.usename,
+        a.query,
+        a.pid
+  FROM pg_stat_activity a
+  JOIN pg_locks l ON l.pid = a.pid
+  WHERE a.application_name = 'psql'
+  ORDER BY a.pid;
+  ```
--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -62,6 +62,9 @@ type TransferTxResult struct {
 	ToEntry     Entry    `json:"To_entry"`
 }
 
+// used for debugging
+// var txKey = struct{}{}
+
 // TransferTx performs a money transfer fro one account to the other
 // It creates a transfer record, add account entries, and update accounts' balance within a single database transaction
 // queries object can be used to call any individual CRUD function
@@ -75,7 +78,10 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
 		// this makes the callback function become a closure, since there are no generics type in Go
 		var err error
 
+		// txName := ctx.Value(txKey)
+
 		// create transfer
+		// fmt.Println(txName, "create transfer")
 		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
 			FromAccountID: arg.FromAccountID,
 			ToAccountID:   arg.ToAccountID,
@@ -86,6 +92,7 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
 		}
 
 		// add account entries
+		// fmt.Println(txName, "create entry 1")
 		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
 			AccountID: arg.FromAccountID,
 			Amount:    -arg.Amount,
@@ -94,6 +101,7 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
 			return err
 		}
 
+		// fmt.Println(txName, "create entry 2")
 		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
 			AccountID: arg.ToAccountID,
 			Amount:    arg.Amount,
@@ -102,7 +110,38 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
 			return err
 		}
 
-		// TODO: update accounts' balance
+		// get account -> update its balance
+		// fmt.Println(txName, "get account 1")
+		// account1, err := q.GetAccountForUpdate(ctx, arg.FromAccountID)
+		// if err != nil {
+		// 	return err
+		// }
+
+		// fmt.Println(txName, "update account 1")
+		// result.FromAccount, err = q.UpdateAccount(ctx, UpdateAccountParams{
+		result.FromAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+			ID:     arg.FromAccountID,
+			Amount: -arg.Amount,
+		})
+		if err != nil {
+			return nil
+		}
+
+		// fmt.Println(txName, "get account 2")
+		// account2, err := q.GetAccountForUpdate(ctx, arg.ToAccountID)
+		// if err != nil {
+		// 	return err
+		// }
+
+		// fmt.Println(txName, "update account 2")
+		// result.ToAccount, err = q.UpdateAccount(ctx, UpdateAccountParams{
+		result.ToAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+			ID:     arg.ToAccountID,
+			Amount: arg.Amount,
+		})
+		if err != nil {
+			return nil
+		}
 
 		return nil
 	})
